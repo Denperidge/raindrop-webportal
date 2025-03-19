1f92698109285519046d5935ce358cbaa77df1f1
Denperidge <denperidge@gmail.com>
2025-03-19 10:28:45 +0100

    WIP: Better favicon fetching

--- .raindrop.js
+++ .raindrop.js
@@ -7,27 +7,63 @@ import { finished } from "stream/promises";
 
 const OUTPUT_FILEPATH = "src/_data/raindrops.json";
 
-const regexGetCollections = RegExp(/(?<=vite-plugin-ssr_pageContext.*?>).*?(?=<\/script>)/)
+const REGEX_GET_COLLECTIONS = RegExp(/(?<=vite-plugin-ssr_pageContext.*?>).*?(?=<\/script>)/)
+const REGEX_GET_ICO = RegExp(/(?<=href=").*?\.ico/);
 
-async function downloadFavicon(url, outputFilename, triedBaseDomain=false) {
+function getBaseUrl(url) {
+    return url.substring(0, url.indexOf("/"));
+}
+
+async function downloadFavicon(url, outputFilename, tried={baseDomain:false, fromHtml:false}) {
     const fullOutputFilename = "src/assets/" + outputFilename + ".ico";
     return new Promise(async (resolve, reject) => {
+        let error;
         try {
-            const data = await fetch(join(url, "favicon.ico"));
+            const requestUrl = url.endsWith(".ico") ?
+                url : join(url, "favicon.ico")
+            console.log("Requesting favicon from " + requestUrl)
+            const data = await fetch(requestUrl);
             // Thanks to antonok on https://stackoverflow.com/a/74722818
             const body = data.body;
-            const stream = createWriteStream(fullOutputFilename);
-            await finished(Readable.fromWeb(body).pipe(stream))
-            resolve(fullOutputFilename)
+            if (data.status < 400) {
+                const stream = createWriteStream(fullOutputFilename);
+                await finished(Readable.fromWeb(body).pipe(stream))
+                resolve(fullOutputFilename);
+            }
         } catch (e) {
-            console.error(`Couldn't finish getting data from ${env.RAINDROP_URL}`);
-            if (!triedBaseDomain) {
-                downloadFavicon(url, outputFilename, true).then((out) => {
-                    resolve(out);
-                })
-            } else {
+            error = e;
+        }
+        if (!tried.fromHtml) {
+            tried.fromHtml = true;
+            const requestUrl = url.endsWith(".ico") ?
+                url.substring(0, url.lastIndexOf("/") + 1) :
+                url;
+            console.log("Determining favicon from " + requestUrl)
+            const data = await fetch(requestUrl);
+            contents = await data.text();
+            console.log(contents)
+            console.log("***")
+            try {
+                const icoPath = contents.match(REGEX_GET_ICO)[0];
+                let newUrl;
+                if (icoPath.startsWith("/")) {
+                    newUrl = join(getBaseUrl(url), icoPath);
+                } else {
+                    newUrl = join(url, icoPath);
+                }
+                downloadFavicon(newUrl, outputFilename, tried).then(resolve);
+            } catch (e) {
+                console.log("Error during determining faivcon from HTML")
+                console.log(`status code: ${data.status}, url: ${url}, outputFilename: ${outputFilename}, tried: ${JSON.stringify(tried)}`);
                 throw e;
             }
+            
+        } else if (!tried.baseDomain) {
+            tried.baseDomain = true;
+            downloadFavicon(getBaseUrl(url), outputFilename, tried).then(resolve);
+        } else if (error) {
+            console.log(`Error whilst downloading favicon. URL: ${url}, outputFilename: ${outputFilename}, tried: ${JSON.stringify(tried)}`)
+            throw error;
         }
     });
 }
@@ -43,7 +79,7 @@ try {
 }
 
 const data = JSON.parse(
-    contents.match(regexGetCollections)[0]
+    contents.match(REGEX_GET_COLLECTIONS)[0]
 );
 const relevantRaindrops = data.pageContext.pageProps.raindrops.items;
 
@@ -58,7 +94,7 @@ writeFile(
 );
 
 relevantRaindrops.forEach(async raindrop => {
-    console.log(
-        await downloadFavicon(raindrop.link, raindrop._id)
-    )
+    downloadFavicon(raindrop.link, raindrop._id).then(output => {
+        console.log(`Downloaded favicon for ${raindrop.link} to ${output}`)
+    }).catch(err => { throw err; });
 });
