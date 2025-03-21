a41ff6a1e78e35071d1f6ac0e743ded85c7a99c6
Denperidge <denperidge@gmail.com>
2025-03-21 15:19:26 +0100

    .raindrop.js changes, favicon styling

--- .raindrop.js
+++ .raindrop.js
@@ -1,42 +1,52 @@
 import 'dotenv/config';
-import downloadFavicon from "favicon-grabber";
+import downloadFavicon, { downloadFaviconFromWebpage } from "favicon-grabber";
 import { env } from "process"
 import { writeFile } from "fs";
 
 const OUTPUT_FILEPATH = "src/_data/raindrops.json";
-
 const REGEX_GET_COLLECTIONS = RegExp(/(?<=vite-plugin-ssr_pageContext.*?>).*?(?=<\/script>)/)
-const REGEX_GET_ICO = RegExp(/(?<=href=").*?\.ico/);
 
+async function getRaindropCollectionData() {
+    let raindropCollectionHtml;
+    try {
+        const data = await fetch (env.RAINDROP_URL);
+        raindropCollectionHtml = await data.text();
+    } catch (e) {
+        console.error(`Couldn't finish getting data from ${env.RAINDROP_URL}`);
+        throw e;
+    }
 
-let raindropCollectionHtml;
-try {
-    const data = await fetch (env.RAINDROP_URL);
-    raindropCollectionHtml = await data.text();
-} catch (e) {
-    console.error(`Couldn't finish getting data from ${env.RAINDROP_URL}`);
-    throw e;
+    const raindropCollectionData = JSON.parse(
+        raindropCollectionHtml.match(
+            REGEX_GET_COLLECTIONS
+        )[0]
+    );
+    const relevantRaindrops = raindropCollectionData.pageContext.pageProps.raindrops.items;
+
+    writeFile(
+        OUTPUT_FILEPATH,
+        JSON.stringify(relevantRaindrops),
+        { encoding: "utf-8" },
+        (e) => {
+            if (e) { throw e; }
+            else { console.log("Done! Wrote to " + OUTPUT_FILEPATH); }
+        }
+    );
+
+    return relevantRaindrops;
 }
 
-const raindropCollectionData = JSON.parse(
-    raindropCollectionHtml.match(
-        REGEX_GET_COLLECTIONS
-    )[0]
-);
-const relevantRaindrops = raindropCollectionData.pageContext.pageProps.raindrops.items;
+const raindrops = await getRaindropCollectionData();
 
-writeFile(
-    OUTPUT_FILEPATH,
-    JSON.stringify(relevantRaindrops),
-    { encoding: "utf-8" },
-    (e) => {
-        if (e) { throw e; }
-        else { console.log("Done! Wrote to " + OUTPUT_FILEPATH); }
+raindrops.forEach(async raindrop => {
+    let downloadFunc = downloadFavicon;
+    if (raindrop.link.includes("transreads", "https://transreads.org/")) {
+        downloadFunc = downloadFaviconFromWebpage        
     }
-);
-
-relevantRaindrops.forEach(async raindrop => {
-    downloadFavicon(raindrop.link, `src/assets/${raindrop._id}%extname%`).then(output => {
+    try {
+        const output = await downloadFunc(raindrop.link, `src/assets/${raindrop._id}%extname%`)
         console.log(`Downloaded favicon for ${raindrop.link} to ${output}`)
-    }).catch(err => { throw err; });
+    } catch (e) {
+        console.log(e)
+    }
 });
